0780edcd2af7bf3471431bc01566efad89e30f3d
Margus Mahler <margus@toggl.com>
2015-02-16 18:58:31 +0200

    added neater copy + some comment in code

--- timeoutclient.go
+++ timeoutclient.go
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ */
+
 package timeoutclient
 
 import (
@@ -20,6 +29,7 @@ func timeoutDialer(connectTimeout time.Duration, readWriteTimeout time.Duration)
 	}
 }
 
+// NewTimeoutClient returns a http.Client, that has a connectTimeout and readWriteTimeout (any time.Duration will do)
 func NewTimeoutClient(connectTimeout time.Duration, readWriteTimeout time.Duration) *http.Client {
 	return &http.Client{
 		Transport: &http.Transport{
